73b1c9adc5534b287305c1efa49fbcfa068178f8
Anikkarki <anikkarki5@gmail.com>
2021-09-30 18:45:39 +0545

    finance website

---
+++ portion/menu.php
@@ -0,0 +1,34 @@
+<header class="header-area">
+<div class="top-header-area">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12 d-flex justify-content-between">
+                        <!-- Logo Area -->
+                        <div class="logo col-md-5 col-sm-12 col-xs-12">
+                          <a href="/"><img src="image/logo.svg" class="img-responsive"></a>
+                        </div>
+                        
+                        <!-- Top Contact Info -->
+                        <div class="top-contact-info d-flex align-items-center  col-md-7 col-sm-12 col-xs-12">
+                            <a href="#" ><img class="logo1"src="image/location.svg" alt=""> <span>Location, Nepal </span></a>
+                            <a href="mailto:info@sfcl.com.np" ><img class="logo2" src="image/mail.svg" alt=""> <span>myemail@here.com.np</span></a>
+                            <a href="tel:057-525100, 01-4212099" style="margin-left: 20px" ; ><img class="logo3" src="image/call.svg" alt=""><span>Phone no here, 01-phn</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   
+   <!--menu section start-->
+        <div class="classynav">
+            <ul>
+               <li class="active"><a href="/">Home</a></li>
+               <li><a href="@">About Us</a></li>
+               <li><a href=".">Loan</a></li>
+               <li><a href="$">Deposite</a></li>
+               <li><a href="%">Services</a></li>
+               <li><a href="!">Resources</a></li>
+               <li><a href="#">Rate</a></li>
+            </ul>
+         </div>
+</header>
